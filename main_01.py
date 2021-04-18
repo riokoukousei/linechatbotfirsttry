7e35edd003d1a68d98a66e894a24926cb902edda
Zamee Mallik <zameemallik@gmail.com>
2021-04-19 04:33:29 +0900

    secont try

--- main_01.py
+++ main_01.py
@@ -10,8 +10,8 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info['USER_ID']
-    messages= TextSendMessage(text="りょう天才！じゃみーよりはしただけどね！")
+    messages= TextSendMessage(text="おれ天才")
     line_bot_api.push_message(USER_ID, messages=messages)
-    
+
 if __name__== "__main__":
     main()
